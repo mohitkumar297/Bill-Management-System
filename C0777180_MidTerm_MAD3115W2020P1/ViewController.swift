30edbaed784b41120d7e05c92e07a6e4284aee7e
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-03-05 01:27:50 -0500

    Initial Commit

---
+++ C0777180_MidTerm_MAD3115W2020P1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0777180_MidTerm_MAD3115W2020P1
+//
+//  Created by Mohit Kumar on 2020-03-05.
+//  Copyright © 2020 Mohit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
