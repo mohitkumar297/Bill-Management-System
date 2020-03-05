dfe4a1181a51b14849db5b26bc980982967c8db0
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-03-05 13:11:06 -0500

    [MK] Added main class

---
+++ C0777180_MidTerm_MAD3115W2020P1/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  C0777180_MidTerm_MAD3115W2020P1
+//
+//  Created by Mohit Kumar on 2020-03-05.
+//  Copyright © 2020 Mohit Kumar. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
+
+
+
