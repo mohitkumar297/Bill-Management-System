8b48260deb2ada5db52ea3c90d2aec2ff17b4222
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-03-05 13:13:49 -0500

    [MK] Added customer class and bill

---
+++ C0777180_MidTerm_MAD3115W2020P1/Customer.swift
@@ -0,0 +1,63 @@
+//
+//  Customer.swift
+//  C0777180_MidTerm_MAD3115W2020P1
+//
+//  Created by Mohit Kumar on 2020-03-05.
+//  Copyright © 2020 Mohit Kumar. All rights reserved.
+//
+
+import Foundation
+
+enum validEmail: Error{
+    case invalidEmail(Problem : String)
+}
+
+class Customer: IDisplay {
+    var customerId: String
+    var firstName: String
+    var lastName: String
+    var fullName: String{
+        return "\(firstName) \(lastName)"
+    }
+    var email: String
+    lazy var bills : [String:Bill]=[:]
+    lazy var totalBill: Double = calculatedBill()
+    
+//https://docs.swift.org/swift-book/LanguageGuide/ErrorHandling.html
+    init(customerId:String, firstName: String, lastName: String, email: String)throws {
+        self.customerId = customerId
+        self.firstName = firstName
+        self.lastName = lastName
+        
+        guard email.isValidEmail(email: email) else {
+            throw validEmail.invalidEmail(Problem: "\(email)")
+        }
+    
+        self.email = email
+    }
+    func calculatedBill() -> Double{
+        var t = 0.0
+        for(_,v) in bills{
+            t = t + v.billAmount
+        }
+        return t
+    }
+    
+    func display(){
+        print("Customer ID : \(customerId)")
+        print("Customer Full Name : \(fullName)")
+        if email.isValidEmail(email: email){
+            print("Customer Email ID : \(email)")}
+        print("         ------------Bill Information-------------    ")
+        print("         ***************************************************")
+        for bill in bills{
+            bill.value.display()
+        }
+        let t = "".formatCurrency(billAmount: calculatedBill())
+        print("                  Total Bill Amount : \(t)")
+        print("         ***************************************************")
+        print(" ")
+    }
+  }
+
+
---
+++ C0777180_MidTerm_MAD3115W2020P1/IDisplay.swift
@@ -0,0 +1,13 @@
+//
+//  IDisplay.swift
+//  C0777180_MidTerm_MAD3115W2020P1
+//
+//  Created by Mohit Kumar on 2020-03-05.
+//  Copyright © 2020 Mohit Kumar. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplay {
+    func display()
+}
