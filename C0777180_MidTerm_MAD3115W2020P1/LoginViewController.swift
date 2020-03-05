8be0f13f287434a446b16d01326032c4da7148f2
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-03-05 02:12:28 -0500

    [MK] Connections added in view controller

--- C0777180_MidTerm_MAD3115W2020P1/LoginViewController.swift
+++ C0777180_MidTerm_MAD3115W2020P1/LoginViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    let email = "mohit@gmail.com"
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var switchSave: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnLogin(_ sender: UIButton) {
+        
+    }
+    
 }
 
